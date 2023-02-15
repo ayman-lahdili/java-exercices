d19acd458acbfa2cd7abe376674ac57d02dd87f4
alahdili <aymanlahdili25@gmail.com>
2023-02-15 11:18:18 -0500

    Renamed and Closed Scanner object

--- Exo1/TestNumber.java
+++ Exo1/TestNumber.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 public class TestNumber{
 
     public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
+        Scanner input=new Scanner(System.in);
 
         System.out.println("Enter number please ");
-        int a=sc.nextInt();
+        int a=input.nextInt();
         System.out.println("Enter an other number please ");
-        int b=sc.nextInt();
+        int b=input.nextInt();
 
         DivNumber isDiv=new DivNumber();
         isDiv.isDivisibly(a, b);
+
+        input.close();
     }
 }
