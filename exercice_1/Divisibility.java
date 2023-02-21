2baec8459c15b19eeefcaca892d10c0deb7edfd0
alahdili <aymanlahdili25@gmail.com>
2023-02-20 20:54:28 -0500

    Ajout de la documentation

--- exercice_1/Divisibility.java
+++ exercice_1/Divisibility.java
@@ -1,45 +1,64 @@
+/** 
+ * Exercice 1 dans le cadre du devoir 1
+ */
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+
+/**
+ * Class principal
+ * @author theGholem
+ * @author ayman-lahdili
+ */
 public class Divisibility{
 
-  private int a, b;
+  private int a, b; // les deux valeurs à comparer
 
+  /**
+   * Constructeur pour la class Divisibility
+   * @param a
+   * @param b
+   */
   public Divisibility(int a, int b){
     this.a=a;
     this.b=b;
   }
 
-  public void isDisvisible(){
-    if(this.a%this.b==0){
+  /**
+   *  Imprime si le nombre a est divisble par le nombre b ou vice-versa
+   */
+  public void isDisvisible() {
+    if ( this.a%this.b == 0 ){
       System.out.println(this.a+" est divisible par "+this.b+" .");
-    }else if(this.a%this.b!=0 && this.b%this.a==0){
+    } else if ( this.a%this.b!=0 && this.b%this.a == 0 ) {
       System.out.println(this.a+" n'est pas divisible par "+this.b+" . \nPar contre "+this.b+" est divisible par "+this.a);
-    }else{
+    } else {
       System.out.println("Aucun des deux n'est divisible par l'autre");
     }
   }
 
 
-
-  public static void main(String[]args){
+  public static void main(String[]args) {
 
     System.out.println("-----> EXO1 \n");
-    Scanner sc=new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-    try{
+    
+    try {
 
       System.out.println("Veuillez saisir le entier nombre svp");
-      int n1=sc.nextInt();
+      int n1 = input.nextInt();
       System.out.println("Veuillez saisir le second entier svp");
-      int n2=sc.nextInt();
+      int n2 = input.nextInt();
 
-      Divisibility isD=new Divisibility(n1,n2);
+      Divisibility isD = new Divisibility(n1,n2);
       isD.isDisvisible();
 
-    }catch(InputMismatchException e){
-      System.out.println("Error : "+e.getMessage());
+    } catch (InputMismatchException e) {
+      System.out.println("Error : "+e.getMessage()+"/nLe type int est attendu");
     }
+
+    input.close();
 
   }
 
