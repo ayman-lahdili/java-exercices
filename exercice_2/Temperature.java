e3b8ff472b7975ac831c052c7b401c7214289b0e
Santiago-Soto-21 <124314815+Santiago-Soto-21@users.noreply.github.com>
2023-02-21 20:01:39 -0500

    Update Temperature.java

--- exercice_2/Temperature.java
+++ exercice_2/Temperature.java
@@ -1,5 +1,22 @@
 package exercice_2;
 
 public class Temperature {
-    
+    private int temperature;
+	
+	public Temperature(int temperature) {
+		this.temperature = temperature;
+	}
+			
+	public int getFahrenheit() {
+		return temperature;
+	}
+	
+	public int getCelsius() {
+		return 5*(temperature - 32)/9;
+	}
+
+	@Override
+	public String toString() {
+		return "La température est de " + getFahrenheit() +" degrés Fahrenheit" + " ou de " + getCelsius() + " degrés Celsius.";
+	}
 }
