5401366e4974d6126ca031b10cfa9c9f2695cb74
alahdili <aymanlahdili25@gmail.com>
2023-02-25 19:21:34 -0500

    Ajout de la documentation

--- exercice_1/Divisibility.java
+++ exercice_1/Divisibility.java
@@ -3,10 +3,13 @@
  */
 import java.util.Scanner;
 import java.util.InputMismatchException;
-
+import java.lang.ArithmeticException;
 
 /**
  * Class principal
+ * Cette classe vérifie si deux nombres entiers sont divisibles ou non
+ * Elle possède une méthode principale qui demande à l'utilisateur d'entrer deux nombres entiers, 
+ * puis vérifie si l'un est divisible par l'autre.
  * @author theGholem
  * @author ayman-lahdili
  */
@@ -24,9 +27,9 @@ public class Divisibility{
     this.b=b;
   }
 
-  /**
+   /**
    * Vérifie si un le nombre a sont divisbles par b
-   * @return boolean
+   * @return true si a est divisible par b, false sinon
    */
   public boolean isDisvisible() {
     if ( this.a%this.b == 0 ){
@@ -36,7 +39,11 @@ public class Divisibility{
     
   }
 
-
+  /**
+   * Méthode main qui permet d'instancier la classe Divisibility avec
+   * des caractères fournis par l'utilisateur et de générer toutes les
+   * permutations possibles.
+   */
   public static void main(String[]args) {
 
     System.out.println("-----> EXO1 \n");
@@ -47,23 +54,35 @@ public class Divisibility{
     do {
       try {
         validInput=true;
-        System.out.println("Veuillez saisir le entier nombre svp");
+        System.out.println("Premier nombre entier: ");
         int n1 = input.nextInt();
-        System.out.println("Veuillez saisir le second entier svp");
+        System.out.println("Second nombre entier: ");
         int n2 = input.nextInt();
 
+        // Vérifier si n1 est divisible par n2
         if ( new Divisibility(n1,n2).isDisvisible() ){
           System.out.println(n1+" est divisible par "+n2+".");
-        } else if ( new Divisibility(n2,n1).isDisvisible() ) {
+        } 
+        // Vérifier si n2 est divisible par n1
+        else if ( new Divisibility(n2,n1).isDisvisible() ) {
           System.out.println(n1+" n'est pas divisible par "+n2+". \nPar contre, "+n2+" est divisible par "+n1+".");
-        } else {
+        } 
+        // Aucun des deux nombres n'est divisible
+        else {
           System.out.println("Aucun des deux nombres n'est divisible");
         }
 
-      } catch (InputMismatchException e) {
+      } 
+      // Si l'utilisateur entre une valeur qui ne correspond pas au type attendu (dans ce cas, un entier). Si cette exception est levée, le programme va appeler la méthode next() de l'objet Scanner pour vider la ligne qui contient l'entrée invalide, et va demander à l'utilisateur d'entrer une nouvelle valeur.
+      catch (InputMismatchException e) {
         input.next();
         validInput = false;
-        System.out.println("Veuiller saisir un nombre entier");
+        System.out.println("Veuiller saisir un nombre entier\n");
+      } 
+      // Cette exception est levée lorsqu'une division par zéro est effectuée. Si cette exception est levée, le programme va afficher un message indiquant à l'utilisateur qu'il doit entrer un nombre différent de zéro et va redemander une entrée valide.
+      catch (ArithmeticException e) {
+        validInput = false;
+        System.out.println("Veuiller saisir un nombre différent de 0!\n");
       }
     } while (validInput == false);
 
--- exercice_3/Permutation.java
+++ exercice_3/Permutation.java
@@ -4,38 +4,77 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 /**
- * Class principal
+ * Classe Permutation qui permet de générer toutes les combinaisons
+ * possibles de trois caractères fournis en entrée.
+ * Les combinaisons sont affichées sur des lignes séparées.
+ * Par exemple, la méthode Permute("a", "b", "c") produira la sortie suivante :
+ * abc
+ * acb
+ * bac
+ * bca
+ * cab
+ * cba
  * @author theGholem
  * @author ayman-lahdili
- */
+ * @version 1.0
+    */
 public class Permutation{
 
-  private String  a, b, c; //les trois lignes à permuter
+  private char a; 
+  private char b; 
+  private char c; //les trois caractères à permuter
 
-  public Permutation(String a, 	String b, String c){
+  /**
+   * Constructeur pour la class Permutation qui initialise les
+   * trois caractères à permuter.
+   * @param a le premier caractère
+   * @param b le deuxième caractère
+   * @param c le troisième caractère
+   */
+  public Permutation(char a, 	char b, char c){
     this.a=a;
     this.b=b;
     this.c=c;
   }
 
+  /**
+   * Génère et affiche les différentes permutations des trois caractères
+   * Les différentes permutations sont affichées sur des lignes séparées.
+   */
   public void Permute(){
-    System.out.println("\nLa permutation est : \n "+this.a+this.b+this.c+"\n"+this.b+this.c+this.a+"\n"+this.c+this.a+this.b+"\n"+this.a+this.c+this.b+"\n"+this.b+this.a+this.c+"\n"+this.c+this.b+""+this.a+"\n");
+    System.out.println( "\nLa permutation est : \n"+this.a+this.b+this.c+
+                                                "\n"+this.b+this.c+this.a+
+                                                "\n"+this.c+this.a+this.b+
+                                                "\n"+this.a+this.c+this.b+
+                                                "\n"+this.b+this.a+this.c+
+                                                "\n"+this.c+this.b+this.a+
+                                                "\n" );
   }
 
+  /**
+   * Méthode main qui permet d'instancier la classe Permutation avec
+   * des caractères fournis par l'utilisateur et de générer toutes les
+   * permutations possibles.
+   * @param args
+   * @throws Exception si une erreur d'entrée/sortie se produit
+   */
   public static void main(String[]args) throws Exception {
-
     System.out.println("-----> EXO3 \n");
-
+    
+    // Lecture des entrées de l'utilisateur
     InputStreamReader isr=new InputStreamReader(System.in);
 		BufferedReader br=new BufferedReader(isr);
 
-    System.out.println("Saisir vos trois lettres svp");
-    String letter1=br.readLine();
-    String letter2=br.readLine();
-    String letter3=br.readLine();
+    System.out.println("Veuiller saisir vos trois lettres: ");
+    System.out.print("1: "); 
+    char letter1=br.readLine().charAt(0);
+    System.out.print("2: "); 
+    char letter2=br.readLine().charAt(0);
+    System.out.print("3: ");
+    char letter3=br.readLine().charAt(0);
 
+    // Génération et affichage des permutations
     Permutation isP=new Permutation(letter1,letter2,letter3);
     isP.Permute();
 
